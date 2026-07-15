ea612b75aa20c3d4581813d9d29578fe8cc77088
Owen Cross <owencross99@gmail.com>
2026-07-15 03:05:18 +0200

    add folder for games list

--- src/frontend/apesdb/src/routes/_app.games.tsx
+++ src/frontend/apesdb/src/routes/_app.games.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { GamesPage } from "../features/games/games-page";
+import { GamesPage } from "../features/games/list-games/games-page";
 
 export const Route = createFileRoute("/_app/games")({
   component: GamesPage,
